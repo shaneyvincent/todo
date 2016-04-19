3d6d26812d21f4c146f7992c315acab2d4f0e0df
Shane Vincent <shaneyshaneshane5@gmail.com>
2016-04-19 15:11:44 -0400

    confi routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :todo_items
+    root 'todo_items#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+
   # root 'welcome#index'
 
   # Example of regular route:
